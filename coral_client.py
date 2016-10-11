53a668a4509d02a0a05ef83ba8d9dce81d51dabf
Gokhan Karaboga <gokhankaraboga@Gokhans-MacBook-Air.local>
2016-10-11 08:58:20 +0300

    First drafts for client

---
+++ coral_client.py
@@ -0,0 +1,67 @@
+import requests
+from urlparse import urljoin
+
+
+class Client(object):
+    def __init__(self, username, password):
+        self.BASE_URL = 'http://localhost:8000/api/v2/'
+        self.SEARCH_URL = urljoin(self.BASE_URL, 'search/')
+        self.AVAILABILITY_URL = urljoin(self.BASE_URL, 'availability/')
+        self.PROVISION_URL = urljoin(self.BASE_URL, 'provision/')
+        self.BOOK_URL = urljoin(self.BASE_URL, 'book/')
+        self.CANCEL_URL = urljoin(self.BASE_URL, 'cancel/')
+        self.BOOKINGS_URL = urljoin(self.BASE_URL, 'bookings/')
+
+        self.username = username
+        self.password = password
+
+    def search(self, search_params):
+        response = requests.get(self.SEARCH_URL, params=search_params,
+                                auth=(self.username, self.password))
+        if response.status_code != 200:
+            raise ValueError('Bad Request')
+        return response.status_code, response.json()
+
+    def availability(self, product_code):
+        response = requests.get(self.AVAILABILITY_URL + product_code,
+                                auth=(self.username, self.password))
+
+        if response.status_code != 200:
+            raise ValueError('Bad Request')
+        return response.json()
+
+    def provision(self, product_code):
+        response = requests.post(self.PROVISION_URL + product_code,
+                                 auth=(self.username, self.password))
+
+        if response.status_code != 200:
+            raise ValueError('Bad Request')
+
+        return response.json()
+
+    def book(self, provision_code):
+        response = requests.post(self.BOOK_URL + provision_code,
+                                 data={'name': '1,Gokhan,Karaboga,adult'},
+                                 auth=(self.username, self.password))
+        if response.status_code != 200:
+            raise ValueError('Bad Request')
+
+        return response.json()
+
+    def cancel(self, book_code):
+        response = requests.post(self.CANCEL_URL + book_code,
+                                 auth=(self.username, self.password))
+
+        if response.status_code != 200:
+            raise ValueError('Bad Request')
+
+        return response.json()
+
+    def bookings(self):
+        response = requests.get(self.BOOKINGS_URL,
+                                auth=(self.username, self.password))
+
+        if response.status_code != 200:
+            raise ValueError('Bad Request')
+
+        return response.json()
---
+++ main.py
@@ -0,0 +1,40 @@
+from coral_client import Client
+import base64
+
+'''
+seach-availability-provision-book-cancel-bookings
+provision, book ve cancel post yapar
+search, availability ve bookings get yapar
+'''
+
+search_params = {'checkin': '2016-11-25', 'checkout': '2016-12-14', 'pax': '1',
+                 'destination_code': '11260', 'client_nationality': 'tr',
+                 'currency': 'USD'}
+
+a = Client(base64.b64decode('Z29raGFuLmthcmFib2dh'),
+           base64.b64decode('WWV0MTIzKys='))
+
+response_1 = a.search(search_params)
+product_code = response_1[1]['results'][0]['products'][0]['code']
+print '\n', 'product code:', product_code
+print 'search json object:', response_1, '\n'
+
+response_2 = a.availability(product_code)
+
+response_3 = a.provision(product_code)
+book_code = response_3["code"]
+print 'book code:', book_code
+
+response_4 = a.book(book_code)
+cancel_code = response_4['code']
+print 'book json object:', response_4, '\n'
+print 'cancel code:', cancel_code
+
+response_5 = a.cancel(cancel_code)
+print 'cancel json object:', response_5, '\n'
+
+response_6 = a.bookings()
+print 'bookings json object:', response_6
+
+# print a.availability(product_code)
+# print a.provision(product_code)
