322f46d0c3d97925b587ebf3433f48ff67fbcdb0
Gokhan Karaboga <gokhankaraboga@Gokhans-MacBook-Air.local>
2016-10-12 15:55:25 +0300

    A few test cases were added.

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import unittest
 from coral_client import Client
 import base64
 
-search_params = {'checkin': '2016-11-25', 'checkout': '2016-12-14', 'pax': '1',
-                 'destination_code': '1c11e', 'client_nationality': 'tr',
+search_params = {'checkin': '2017-01-28', 'checkout': '2017-01-30', 'pax': '1',
+                 'destination_code': '11260', 'client_nationality': 'tr',
                  'currency': 'USD'}
 
-search_params2 = {'checkin': '2016-12-25', 'checkout': '2016-12-27',
+search_params2 = {'checkin': '2017-02-23', 'checkout': '2017-02-25',
                   'pax': '1',
-                  'destination_code': '1c11e', 'client_nationality': 'tr',
+                  'destination_code': '11260', 'client_nationality': 'tr',
                   'currency': 'USD'}
 
 a = Client(base64.b64decode('Z29raGFuLmthcmFib2dh'),
@@ -24,8 +24,6 @@ class TestClass(unittest.TestCase):
     search_response = a.search(search_params)
     availability_response = a.availability(product_code)
     provision_response = a.provision(product_code)
-    book_code = provision_response[1]['code']
-    book_response = a.book(book_code)
 
     def test_search(self):
         self.assertEqual(self.search_response[0], 200)
@@ -38,18 +36,27 @@ class TestClass(unittest.TestCase):
         self.assertEqual(self.availability_response[0], 200)
         self.assertTrue(
             isinstance(self.availability_response[1]['rooms'], list))
+        self.assertGreater(float(self.availability_response[1]['price']), 0)
 
     def test_provision(self):
         self.assertEqual(self.provision_response[0], 200)
+        self.assertIn('rooms', self.provision_response[1])
+        self.assertIsNot(self.provision_response[1]['currency'], 'null')
 
     def test_book(self):
-        self.assertEqual(self.book_response[0], 200)
+        book_code = self.provision_response[1]['code']
+        book_response = a.book(book_code)
+        self.assertEqual(book_response[0], 200)
+        self.assertDictContainsSubset({'pay_at_hotel': False},
+                                      book_response[1])
 
     def test_cancel(self):
-        book_code = a.provision(product_code2)[1]['code']
-        cancel_code = a.book(book_code)[1]['code']
-        self.assertEqual(a.cancel(cancel_code)
+        book_code2 = a.provision(product_code2)[1]['code']
+        cancel_code = a.book(book_code2)[1]['code']
+        cancel_response = a.cancel(cancel_code)
+        self.assertEqual(cancel_response
                          [0], 200)
+        self.assertEqual(cancel_response[2].headers['allow'], 'POST, OPTIONS')
 
     def test_bookings(self):
         self.assertEqual(a.bookings()[0], 200)
